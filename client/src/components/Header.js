737753a2a24eb17d111936b77607e0ceb2e1d73f
adorgan <adorgan@gmail.com>
2021-03-25 18:46:50 -0500

    first commit

---
+++ client/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+            Hello
+        </header>
+    )
+}
+
+export default Header
+
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://adorgan:rAr6uMCpkim-S7f@cluster0.oj90x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}

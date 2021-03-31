adc48d190b70a87c2a9cddf12e16916e124e355c
adorgan <adorgan@gmail.com>
2021-03-30 19:59:48 -0500

    added postbuild

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-
+const path = require('path');
 const app = express();
 
 app.use(bodyParser.json());
@@ -17,6 +17,16 @@ mongoose
   })
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
+
+//serve static assets if in production
+if(process.env.NODE_ENV === 'production'){
+  //set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'client','build','index.html'))
+  });
+}
 
 const port = process.env.PORT || 5000;
 

6ed77ead146c3c07ded160bbd1bfa1fbfbdceac1
Andrew Dorgan <adorgan@gmail.com>
2021-06-28 18:20:47 -0500

    d

--- server/index.js
+++ server/index.js
@@ -122,10 +122,14 @@ app.post("/arm-bike", (req, res) => {
 //serve static assets if in production
 if (process.env.NODE_ENV === "production") {
     //set static folder
-    app.use(express.static("/../client/build"));
+    app.use(express.static("client/build"));
 
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
+        let url = path.join(__dirname, "../client/build", "index.html");
+        if (!url.startsWith("/app/"))
+            // since we're on local windows
+            url = url.substring(1);
+        res.sendFile(url);
     });
 }
 

d14eaf94d91af438a689f44827fe53812e6a0e47
Andrew Dorgan <adorgan@gmail.com>
2021-06-28 19:05:27 -0500

    a

--- server/index.js
+++ server/index.js
@@ -122,7 +122,7 @@ app.post("/arm-bike", (req, res) => {
 //serve static assets if in production
 if (process.env.NODE_ENV === "production") {
     //set static folder
-    app.use(express.static("client/build"));
+    app.use('../', express.static("client/build"));
 
     app.get("*", (req, res) => {
         let url = path.join(__dirname, "../client/build", "index.html");

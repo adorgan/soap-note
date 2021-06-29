99369944f55be2be2dac23d2adc4dddbcc443804
Andrew Dorgan <adorgan@gmail.com>
2021-06-28 19:18:46 -0500

    d

--- server/index.js
+++ server/index.js
@@ -124,12 +124,10 @@ if (process.env.NODE_ENV === "production") {
     //set static folder
     app.use('../', express.static("client/build"));
 
+    const root = path.join(__dirname, "../client", "build");
+    app.use(express.static(root));
     app.get("*", (req, res) => {
-        let url = path.join(__dirname, "../client/build", "index.html");
-        if (!url.startsWith("/app/"))
-            // since we're on local windows
-            url = url.substring(1);
-        res.sendFile(url);
+        res.sendFile("index.html", { root });
     });
 }
 

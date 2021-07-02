ba2fda9518f6b9a18784bb3c0a8d738477e26046
Andrew Dorgan <adorgan@gmail.com>
2021-07-01 20:18:46 -0500

    added JSdoc comments

--- server/controllers/therEx/armbike.js
+++ server/controllers/therEx/armbike.js
@@ -1,19 +1,33 @@
-const makeList = (arr) => {
-    let listStr = "";
-    if (arr.length === 1) {
-        listStr = arr[0];
-    } else if (arr.length === 2) {
-        listStr = `${arr[0]} and ${arr[1]}`;
+/**
+ * This makes a comma separated list of strings.
+ * Ex. ["one", "two", "three"] -> "one, two, and three"
+ * 
+ * @param {string[]} stringArray array of strings to be formatted as list
+ * @returns {string} comma separated list of strings
+ */
+const makeList = (stringArray) => {
+    let stringList = "";
+    if (stringArray.length === 1) {
+        stringList = stringArray[0];
+    } else if (stringArray.length === 2) {
+        stringList = `${stringArray[0]} and ${stringArray[1]}`;
     } else {
-        var len = arr.length;
+        const len = stringArray.length;
         for (let i = 0; i < len - 1; i++) {
-            listStr += `${arr[i]}, `;
+            stringList += `${stringArray[i]}, `;
         }
-        listStr += `and ${arr[len - 1]}`;
+        stringList += `and ${stringArray[len - 1]}`;
     }
-    return listStr;
+    return stringList;
 };
 
+/**
+ * This returns a string indicating how much help the therapist provided for the activity
+ * 
+ * @param {string} client string to describe the patient
+ * @param {string} assistLevel FIM level string
+ * @returns {string} string describing assistance provided
+ */
 const assistBlurb = (client, assistLevel) => {
     const assistDict = {
         independent: "The " + client + " completed the activity independently.",
@@ -36,6 +50,12 @@ const assistBlurb = (client, assistLevel) => {
     return assistDict[assistLevel];
 };
 
+/**
+ * This creates a narrative summary of the arm bike and sends back in response
+ * @param {Object} req 
+ * @param {Object} res 
+ * @returns {JSON} json-formatted response
+ */
 const createArmBike = (req, res) => {
     const goalStr = makeList(req.body.goals);
     const impairmentStr = makeList(req.body.impairments);
@@ -49,7 +69,7 @@ const createArmBike = (req, res) => {
     from BUE strengthening and gross motor activities to maximize their independence 
     with ADLs prior to discharge.`;
 
-    res.json(armBikeStr);
+    return res.json(armBikeStr);
 }
 
 module.exports = createArmBike;

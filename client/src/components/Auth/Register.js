4bd2a518b5ef7b9333ac6b893661a2fe9744d403
Andrew Dorgan <adorgan@gmail.com>
2021-09-01 21:12:47 -0500

    commented more

--- client/src/components/Auth/Register.js
+++ client/src/components/Auth/Register.js
@@ -1,72 +1,72 @@
-import { useState, useReducer } from "react";
-import { Redirect } from "react-router-dom";
-import postData from "../../utils/postRequest";
+// import { useState, useReducer } from "react";
+// import { Redirect } from "react-router-dom";
+// import postData from "../../utils/postRequest";
 
-const defaultFormState = {
-    email: "",
-    password: "",
-};
+// const defaultFormState = {
+//     email: "",
+//     password: "",
+// };
 
-const formReducer = (state, event) => {
-    if (event.reset) {
-        return defaultFormState;
-    }
-    return {
-        ...state,
-        [event.name]: event.value,
-    };
-};
+// const formReducer = (state, event) => {
+//     if (event.reset) {
+//         return defaultFormState;
+//     }
+//     return {
+//         ...state,
+//         [event.name]: event.value,
+//     };
+// };
 
-export default function Register() {
-    const [formData, setFormData] = useReducer(formReducer, defaultFormState);
-    const [success, setSuccess] = useState(false);
-    const handleChange = (event) => {
-        setFormData({
-            name: event.target.name,
-            value: event.target.value,
-        });
-    };
+// export default function Register() {
+//     const [formData, setFormData] = useReducer(formReducer, defaultFormState);
+//     const [success, setSuccess] = useState(false);
+//     const handleChange = (event) => {
+//         setFormData({
+//             name: event.target.name,
+//             value: event.target.value,
+//         });
+//     };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        postData('/register', formData).then(data=>{
-            if(data === "success"){
-                setSuccess(true);
-            }
-            else{
-                setSuccess(false);
-            }
-        })
-    };
+//     const handleSubmit = (e) => {
+//         e.preventDefault();
+//         postData('/register', formData).then(data=>{
+//             if(data === "success"){
+//                 setSuccess(true);
+//             }
+//             else{
+//                 setSuccess(false);
+//             }
+//         })
+//     };
 
-    if(success){
-        return <Redirect to="/login"/>;
-    }
+//     if(success){
+//         return <Redirect to="/login"/>;
+//     }
 
-    return (
-        <>
-            <h3>Register</h3>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="email">Email</label>
-                    <input
-                        id="email"
-                        name="email"
-                        type="email"
-                        onChange={handleChange}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="password">Password</label>
-                    <input
-                        id="password"
-                        name="password"
-                        type="password"
-                        onChange={handleChange}
-                    />
-                </div>
-                <button type="submit">Register</button>
-            </form>
-        </>
-    );
-}
+//     return (
+//         <>
+//             <h3>Register</h3>
+//             <form onSubmit={handleSubmit}>
+//                 <div>
+//                     <label htmlFor="email">Email</label>
+//                     <input
+//                         id="email"
+//                         name="email"
+//                         type="email"
+//                         onChange={handleChange}
+//                     />
+//                 </div>
+//                 <div>
+//                     <label htmlFor="password">Password</label>
+//                     <input
+//                         id="password"
+//                         name="password"
+//                         type="password"
+//                         onChange={handleChange}
+//                     />
+//                 </div>
+//                 <button type="submit">Register</button>
+//             </form>
+//         </>
+//     );
+// }
